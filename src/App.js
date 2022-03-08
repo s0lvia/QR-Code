085424b0ff630ae4f598e2a6c132863e74798391
chinwendu <conuoha@infinion.co>
2022-03-08 12:37:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import image from './assets/image-qr.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img className = "image" src ={image} alt="qr-code"/>
+        <p className="text1"> Improve your front-end skills by building projects</p>
+        <p className="text2"> Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
     </div>
   );
 }
